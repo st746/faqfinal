99e94e945a754d2d3296bd5a44e2c635e81a8b9d
Shraddha Thakkar <st746@njit.edu>
2019-05-11 16:14:46 -0400

    Added userprofile migration model and updated the userseeder to create profile

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -449,6 +449,7 @@ class ComposerStaticInit8a61a8175d5d1ada1a60da3d5e21d628
         'App\\Http\\Middleware\\TrimStrings' => __DIR__ . '/../..' . '/app/Http/Middleware/TrimStrings.php',
         'App\\Http\\Middleware\\TrustProxies' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustProxies.php',
         'App\\Http\\Middleware\\VerifyCsrfToken' => __DIR__ . '/../..' . '/app/Http/Middleware/VerifyCsrfToken.php',
+        'App\\Profile' => __DIR__ . '/../..' . '/app/Profile.php',
         'App\\Providers\\AppServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AppServiceProvider.php',
         'App\\Providers\\AuthServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AuthServiceProvider.php',
         'App\\Providers\\BroadcastServiceProvider' => __DIR__ . '/../..' . '/app/Providers/BroadcastServiceProvider.php',
